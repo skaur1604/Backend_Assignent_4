5e545f7144bd63949552f6e47f2bd78a06a63923
skaur1604 <skaur1604@academic.rrc.ca>
2025-11-04 19:32:12 -0600

    create logger.ts file and add winston logging

---
+++ src/api/v1/utils/logger.ts
@@ -0,0 +1,24 @@
+import winston from 'winston';
+import path from 'path';
+import fs from 'fs';
+
+
+const logDir = path.join(__dirname, '../../logs');
+if (!fs.existsSync(logDir)) {
+    fs.mkdirSync(logDir);
+}
+
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.combine(
+        winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+        winston.format.printf(({ timestamp, level, message }) => `${timestamp} [${level.toUpperCase()}]: ${message}`)
+    ),
+    transports: [
+        new winston.transports.Console(),
+        new winston.transports.File({ filename: path.join(logDir, 'error.log'), level: 'error' }),
+        new winston.transports.File({ filename: path.join(logDir, 'combined.log') }),
+    ],
+});
+
+export default logger;
